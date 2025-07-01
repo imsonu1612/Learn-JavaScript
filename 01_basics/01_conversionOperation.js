247671c68e2be4a624bacbef1eacaf17a8adf1e9
sonu kumar <sonuyadav930822@gmail.com>
2025-07-02 01:27:31 +0530

    type casting

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,44 @@
+// let score =  "22v";
+// let score =  null;
+// let score =  undefined; 
+// let score = true;;
+
+
+//console.log(typeof(score)); // "22"v 
+// console.log(typeof score); // "number"
+
+
+// let valueinNumber = Number(score);
+// console.log(typeof valueinNumber); // "number"
+
+// console.log(valueinNumber); // NaN
+
+
+// NaN means Not a Number
+// NaN is a special value in JavaScript that represents an invalid number.
+
+// Note:-
+/*
+1. "33" will be converted to 33
+2. "22v" will be converted to NaN
+3. null will be converted to 0
+4. undefined will be converted to NaN
+5. true will be converted to 1
+6. false will be converted to 0
+7. "" (empty string) will be converted to 0
+*/
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "Sonu Kumar";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); // true
+
+// Note:-
+/*
+1. 0 will be converted to false
+2. "" (empty string) will be converted to false
+3. null will be converted to false
+
+*/
