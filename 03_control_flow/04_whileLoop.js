e132ce7aef007de7faaddac898a6036fb3fac164
sonu kumar <sonuyadav930822@gmail.com>
2025-07-11 02:19:36 +0530

    while and do while loops

---
+++ 03_control_flow/04_whileLoop.js
@@ -0,0 +1,13 @@
+// while loop---
+let i = 0;
+while (i <= 10) { // condition to check
+    // console.log(i);
+    i = i + 2; // incrementing by 2
+}
+
+// do while loop---
+let j = 0;
+do {
+    // console.log(j); // 
+    j = j + 2; // incrementing by 2
+} while (j <= 10);// condition to check
